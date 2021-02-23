f6e5eab8658d0e7c2d5c5bfababcfbf8d6692285
comedsoons <67965566+comedsoons@users.noreply.github.com>
2021-02-23 16:31:08 +0200

    hm3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const windowImg = React.createElement(
+    "img",
+    {
+        src: "https://images.unsplash.com/photo-1611802730713-94dce40f9070?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+        alt: "windows image"
+    },
+)
+const headline = React.createElement(
+    "h1",
+    {},
+    "Lorem ipsum dolor, sit amet.",
+)
+const box = React.createElement(
+    "div",
+    {
+        children: [windowImg, headline],
+    },
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(box, document.getElementById("root"));
